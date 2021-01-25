6288813d8a8ea238d0ba2b70df3a336bf8c2f082
moonlan <syy2108026294@gmail.com>
2021-01-25 14:33:54 +0800

    初始化项目

---
+++ educational-system-gen/src/main/java/edu/hubu/edusysgen/CMXSGen.java
@@ -0,0 +1,12 @@
+package edu.hubu.edusysgen;
+
+/**
+ * @author moonlan
+ * date 2021/1/25 下午2:07
+ */
+@SuppressWarnings(value = "unused")
+public class CMXSGen {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ educational-system-gen/src/main/java/edu/hubu/edusysgen/TbGen.java
@@ -0,0 +1,12 @@
+package edu.hubu.edusysgen;
+
+/**
+ * @author moonlan
+ * date 2021/1/25 下午2:05
+ */
+@SuppressWarnings(value = "unused")
+public class TbGen {
+    public static void main(String[] args) {
+        
+    }
+}
